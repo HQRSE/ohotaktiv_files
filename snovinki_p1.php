c43aee982b55923d90a8ee75189f095146a2186c
HQRSE <60251201+HQRSE@users.noreply.github.com>
2020-08-14 11:35:14 +0300

    Update snovinki_p1.php

--- snovinki_p1.php
+++ snovinki_p1.php
@@ -2,7 +2,7 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Очистка старых дат");
 /* Очищает свойство sНовинка, если там указана старая дата */
-/* Работает в паре с snovinki_p2.php */
+/* Затем снимает бейдж "Новинка" */
 ?>
 
 <?
@@ -17,16 +17,48 @@ $resItem = CIBlockElement::GetList(
 	array( "ACTIVE"=>"Y", "IBLOCK_ID"=>$iblock_id, ">PROPERTY_SNOVINKI"=>"0"/*, "ID"=>14509*/), // Filter
 	false, // GroupBy
 	false, // NavStartParams
-	array("ID", "PROPERTY_SNOVINKI") // SelectFields
+	array("ID", "PROPERTY_SNOVINKI", "PROPERTY_LABELS") // SelectFields
 );
 while($arItem = $resItem->Fetch())
 {
 	$ELEMENT_ID = $arItem['ID'];
-	if ($cur_date > $arItem['PROPERTY_SNOVINKI_VALUE']) { // Если установленная дата уже старая
+	if ($cur_date > $arItem['PROPERTY_SNOVINKI_VALUE']) { // Если установленная дата старая
 		CIBlockElement::SetPropertyValuesEx($ELEMENT_ID, false, array("SNOVINKI" => "")); // Удаляем ее
-		/* Где-то моя механика не будет уже ставить бейдж. Я не смог найти файл, где определена эта функция */
-		/* Она смотрит на SNOVINKI и если там что-то есть, то она ставит бейдж "Новинка" */
+		/* Где-то моя механика не будет уже ставить бейдж */
+		/* Вчера она работала, а сегодня нет, так что я допилил тут до конца */
+		$VALUES = array();
+		$res = CIBlockElement::GetProperty($iblock_id, $ELEMENT_ID, "sort", "asc", array("CODE" => "LABELS"));
+		while ($ob = $res->GetNext())
+		{
+			$VALUES[] = $ob['VALUE'];
+		}
+		//print_r($VALUES);
+		unset($VALUES[0]); // Убираем из массива значение "Новинка" (бейдж)
+		CIBlockElement::SetPropertyValuesEx( // Сохраняем
+		$ELEMENT_ID,
+		$IBLOCK_ID,
+		array(
+			"LABELS" => $VALUES,
+		)
+		);
+	} else {
+		$VALUES = array();
+		$res = CIBlockElement::GetProperty($iblock_id, $ELEMENT_ID, "sort", "asc", array("CODE" => "LABELS"));
+		while ($ob = $res->GetNext())
+		{
+			$VALUES[] = $ob['VALUE'];
+		}
+		$VALUES[] = '37235'; // Добавляем в массив значение "Новинка" (бейдж)
+		//print_r($VALUES);
+		CIBlockElement::SetPropertyValuesEx( // Сохраняем
+		$ELEMENT_ID,
+		$IBLOCK_ID,
+		array(
+			"LABELS" => $VALUES,
+		)
+		);
 	}
+	//print_r($arItem);
 }
 
 ?>
