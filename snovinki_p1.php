fe7a6ab38b09a79886a1f7824147795e1fc100f4
HQRSE <60251201+HQRSE@users.noreply.github.com>
2020-08-14 10:50:15 +0300

    Update snovinki_p1.php

--- snovinki_p1.php
+++ snovinki_p1.php
@@ -1,38 +1,32 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Title");
+$APPLICATION->SetTitle("Очистка старых дат");
+/* Очищает свойство sНовинка, если там указана старая дата */
+/* Работает в паре с snovinki_p2.php */
 ?>
 
 <?
-/* *** */
-
-
-/* *** */
-CModule::IncludeModule("iblock");
-CModule::IncludeModule("catalog");
-
-// Текущее время:
+// Текущее время
 $cur_date = date('Y-m-d\TH:i:s');
-echo $cur_date."<br>";
-
-$iblock_id = 10; // основной инфоблок
-
-// перебираем все активные элементы нашего инфоблока
+	echo "cur_date: ".$cur_date."<br>";
+$iblock_id = 10; // Основной инфоблок 
+ 
+// Перебираем товары где есть SNOVINKI
 $resItem = CIBlockElement::GetList(
-	array("PROPERTY_SNOVINKI"=>"asc"), // Order
-	array( "ACTIVE"=>"Y", "IBLOCK_ID"=>$iblock_id, /*">PROPERTY_SNOVINKI"=>"0"*/), // Filter
+	array(/*"PROPERTY_SNOVINKI"=>"asc"*/), // Order
+	array( "ACTIVE"=>"Y", "IBLOCK_ID"=>$iblock_id, ">PROPERTY_SNOVINKI"=>"0"/*, "ID"=>14509*/), // Filter
 	false, // GroupBy
 	false, // NavStartParams
-	array("ID", "IBLOCK_ID", "PROPERTY_SNOVINKI") // SelectFields
+	array("ID", "PROPERTY_SNOVINKI") // SelectFields
 );
 while($arItem = $resItem->Fetch())
 {
-	//if ($cur_date > $arItem['PROPERTY_SNOVINKI_VALUE']) {
-	//CIBlockElement::SetPropertyValuesEx($arItem['ID'], false, array("SNOVINKI" => ""));
-	//}
-
-	echo "id: ".$arItem['ID']." - s: ".$arItem['PROPERTY_SNOVINKI_VALUE']."<br>";
-	//print_r($arItem);
+	$ELEMENT_ID = $arItem['ID'];
+	if ($cur_date > $arItem['PROPERTY_SNOVINKI_VALUE']) { // Если установленная дата уже старая
+		CIBlockElement::SetPropertyValuesEx($ELEMENT_ID, false, array("SNOVINKI" => "")); // Удаляем ее
+		/* Где-то моя механика не будет уже ставить бейдж. Я не смог найти файл, где определена эта функция */
+		/* Она смотрит на SNOVINKI и если там что-то есть, то она ставит бейдж "Новинка" */
+	}
 }
 
 ?>
