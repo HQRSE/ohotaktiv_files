af24a5355da3d8266a36d849d395b567b73bc8b0
HQRSE <60251201+HQRSE@users.noreply.github.com>
2020-09-29 13:18:25 +0300

    Update add_photo_parser.php
    Охуенно работает, но надо оптимизировать

--- add_photo_parser.php
+++ add_photo_parser.php
@@ -3,23 +3,41 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Пакетная загрузка фото");
 require_once($_SERVER['DOCUMENT_ROOT']."/bitrix/modules/main/include/prolog_before.php");
 CModule::IncludeModule("iblock");
-
-// 48, 36, 486, 477, 947, 1148, 69, 1104, 1115, 161, 205, 146, 349, 1765, 57, 210, 68, 154, 2613 - все кроме зипа и рыбалки
-// 861 - рыбалка
-
-// Коды из имен файлов/папок: 00195076, 00195077
-// Их ID: 272450, 272452
 ?>
 <main class="catalog-page category-catalog-page quantity_page_style centering" id="start">
 <?
-/* *** */
 $IBLOCK = 10;
 $PROPERTY_CODE = 'MORE_PHOTO';
-$arr_code = array('00001867'); // real
-$count = count($arr_code);
+
+$glob_dir = glob(Bitrix\Main\Application::getDocumentRoot().'/12dev/add_photo_parser/pics/*/', GLOB_ONLYDIR); // после pics надо любой каталог типа
+if ($glob_dir) {
+	$d = 0;
+	while (count($glob_dir) > $d) {
+		$code_dir = basename($glob_dir[$d]);
+		echo "basename_dir_".$d.": ".$code_dir."<br>";
+		$full_codes[] = $code_dir;
+	$d++;
+	}
+}
+
+$glob_file = glob(Bitrix\Main\Application::getDocumentRoot().'/12dev/add_photo_parser/pics/*.{jpg,png,gif,PNG,JPG,GIF,bmp,BMP,jpeg,JPEG}', GLOB_BRACE);
+if ($glob_file) {
+	$minus_array = array(".","jpg","png","gif","PNG","JPG","GIF","bmp","BMP","jpeg","JPEG");
+	$f = 0;
+	while (count($glob_file) > $f) {
+		$code_file = str_replace($minus_array, "", basename($glob_file[$f]));
+		echo "basename_file_".$f.": ".$code_file."<br>";
+		$full_codes[] = $code_file;
+	$f++;
+	}
+}
+
+print_r($full_codes);
+
+$count = count($full_codes);
 $i = 0;
 while ($i < $count) {
-$code = $arr_code[$i];
+$code = $full_codes[$i];
 	$results = $DB->Query("SELECT IBLOCK_ELEMENT_ID FROM b_iblock_element_property WHERE VALUE='$code' AND DESCRIPTION='Код'");
 		while ($row = $results->Fetch())
 		{
@@ -30,18 +48,18 @@ $code = $arr_code[$i];
 				if($ar_props = $db_props->Fetch()) {
 					if (empty($ar_props['VALUE'])) {
 						/* go search photo */
-						$glob = glob(Bitrix\Main\Application::getDocumentRoot().'/12dev/add_photo_parser/pics/'.$code.'/*.{jpg,png,gif,PNG,JPG,GIF,bmp,BMP,jpeg,JPEG}', GLOB_BRACE); // после pics надо любой каталог типа
-						if ($glob) {
-							$c = 0;
-							while (count($glob) > $c) {
-								$pic = CFile::MakeFileArray($glob[$c]);
+						$glob_dir = glob(Bitrix\Main\Application::getDocumentRoot().'/12dev/add_photo_parser/pics/'.$code.'/*.{jpg,png,gif,PNG,JPG,GIF,bmp,BMP,jpeg,JPEG}', GLOB_BRACE); // после pics надо любой каталог типа
+						if ($glob_dir) {
+							$d = 0;
+							while (count($glob_dir) > $d) {
+								$pic = CFile::MakeFileArray($glob_dir[$d]);
 								CIBlockElement::SetPropertyValueCode($el, $PROPERTY_CODE, $pic);
-							$c++;
+							$d++;
 							}
-						} else {
-							$glob = glob(Bitrix\Main\Application::getDocumentRoot().'/12dev/add_photo_parser/pics/'.$code.'.{jpg,png,gif,PNG,JPG,GIF,bmp,BMP,jpeg,JPEG}', GLOB_BRACE);
-							//print_r($glob);
-							$pic = CFile::MakeFileArray($glob[0]);
+						} 
+						$glob_file = glob(Bitrix\Main\Application::getDocumentRoot().'/12dev/add_photo_parser/pics/'.$code.'.{jpg,png,gif,PNG,JPG,GIF,bmp,BMP,jpeg,JPEG}', GLOB_BRACE);
+						if ($glob_file) {
+							$pic = CFile::MakeFileArray($glob_file[0]);
 							CIBlockElement::SetPropertyValueCode($el, $PROPERTY_CODE, $pic);
 						}
 						/* off search photo */
@@ -56,5 +74,6 @@ $i++;
 ?>
 
 </main>
+
 <br>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
